79b7e922d8da539ee4fcb8e16ffa37786820766b
EmmaJennings <ejennings13@my.bcit.ca>
2025-10-17 22:59:54 -0700

    changed favicon and added MVP Page to main page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import React from "react";
+import MVPPage from "./mvp/page";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <MVPPage />
     </div>
   );
 }
