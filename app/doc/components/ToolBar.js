9f03f395f632bd3899442c33d518c8003b4154e3
EmmaJennings <ejennings13@my.bcit.ca>
2025-10-18 20:35:11 -0700

    Moving the ToolBar into its own module

---
+++ app/doc/components/ToolBar.js
@@ -0,0 +1,57 @@
+import styles from "../mvp.module.css";
+
+
+export default function ToolBar() {
+    return (
+        
+        <div className={styles.toolBar}>
+            <div className={styles.toolbarInner}>
+            <div className={styles.toolbarLeft}>
+                <button className={styles.toolButton}>
+                <img
+                    src="/icons/sideBarIcon.svg"
+                    className={styles.toolbarIcon}
+                />
+                </button>
+
+                <button className={styles.toolButton}>
+                <img
+                    src="/icons/sideCompareIcon.svg"
+                    className={styles.toolbarIcon}
+                />
+                </button>
+                <button className={styles.toolButton}>
+                <img src="/icons/searchIcon.svg" className={styles.toolbarIcon} />
+                </button>
+            </div>
+
+            <div className={styles.toolbarRight}>
+                <button className={styles.toolButton}>
+                <img
+                    src="/icons/highlightIcon.svg"
+                    className={styles.toolbarIcon}
+                />
+                </button>
+                <button className={styles.toolButton}>
+                <img src="/icons/textIcon.svg" className={styles.toolbarIcon} />
+                </button>
+                <button className={styles.toolButton}>
+                <img src="/icons/addPicIcon.svg" className={styles.toolbarIcon} />
+                </button>
+                <button className={styles.toolButton}>
+                <img src="/icons/printIcon.svg" className={styles.toolbarIcon} />{" "}
+                </button>
+                <button className={styles.toolButton}>
+                <img
+                    src="/icons/downloadIcon.svg"
+                    className={styles.toolbarIcon}
+                />
+                </button>
+                <button className={styles.toolButton}>
+                <img src="/icons/moreIcon.svg" className={styles.toolbarIcon} />
+                </button>
+            </div>
+            </div>
+        </div>
+    ); 
+}
--- app/doc/page.js
+++ app/doc/page.js
@@ -1,67 +1,20 @@
 import React from "react";
 import DocScreen from "./components/DocScreen";
 import styles from "./mvp.module.css";
+import ToolBar from "./components/ToolBar";
 
 export default function MVPPage() {
   return (
-    <main className={styles.container}>
-
-      <div className={styles.toolBar}>
-        <div className={styles.toolbarInner}>
-          <div className={styles.toolbarLeft}>
-            <button className={styles.toolButton}>
-              <img
-                src="/icons/sideBarIcon.svg"
-                className={styles.toolbarIcon}
-              />
-            </button>
-
-            <button className={styles.toolButton}>
-              <img
-                src="/icons/sideCompareIcon.svg"
-                className={styles.toolbarIcon}
-              />
-            </button>
-            <button className={styles.toolButton}>
-              <img src="/icons/searchIcon.svg" className={styles.toolbarIcon} />
-            </button>
+    <div>
+      <ToolBar />
+      <main className={styles.container}>
+        <section className={styles.viewerArea}>
+          <div className={styles.viewerCenter}>
+            <DocScreen />
           </div>
-
-          <div className={styles.toolbarRight}>
-            <button className={styles.toolButton}>
-              <img
-                src="/icons/highlightIcon.svg"
-                className={styles.toolbarIcon}
-              />
-            </button>
-            <button className={styles.toolButton}>
-              <img src="/icons/textIcon.svg" className={styles.toolbarIcon} />
-            </button>
-            <button className={styles.toolButton}>
-              <img src="/icons/addPicIcon.svg" className={styles.toolbarIcon} />
-            </button>
-            <button className={styles.toolButton}>
-              <img src="/icons/printIcon.svg" className={styles.toolbarIcon} />{" "}
-            </button>
-            <button className={styles.toolButton}>
-              <img
-                src="/icons/downloadIcon.svg"
-                className={styles.toolbarIcon}
-              />
-            </button>
-            <button className={styles.toolButton}>
-              <img src="/icons/moreIcon.svg" className={styles.toolbarIcon} />
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <section className={styles.viewerArea}>
-        <div className={styles.viewerCenter}>
-          <DocScreen />
-        </div>
-      </section>
-      
-    </main>
+        </section>
+        
+      </main>
+    </div>
   );
 }
