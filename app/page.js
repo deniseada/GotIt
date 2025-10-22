71045826fb6620d9448952a631c0a6bf35de54dd
Denise Aquino <daquino10@my.bcit.ca>
2025-10-21 20:02:52 -0700

    removed imports from main page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,4 @@
 import React from "react";
-import Image from "next/image";
-import styles from "./page.module.css";
-import NavBar from "./doc/components/NavBar";
-import MVPPage from "./doc/page";
-import SideBar from "./doc/components/sideBar";
 
 export default function Home() {
   return <div></div>;
