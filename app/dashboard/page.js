9b301eb5aa83d3b6991ee977fd663559d36d4a5a
EmmaJennings <ejennings13@my.bcit.ca>
2025-10-24 17:02:31 -0700

    Adding to dashboard page, importing NavBar and checking the page displays something.

---
+++ app/dashboard/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+import NavBar from "../doc/components/NavBar";
+
+export default function Dashboard() {
+    return <div>
+        <NavBar />
+        <p>Hi there from the dashboard!</p>
+    </div>;
+}
