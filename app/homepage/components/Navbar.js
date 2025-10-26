65fa3b740304d67a2e358dfaa2685df749289162
patcha-d3 <psricome@my.bcit.ca>
2025-10-25 18:03:11 -0700

    made and imported components with css styling

---
+++ app/homepage/components/Navbar.js
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import styles from "../homepage.module.css";
+
+/**
+ * Navbar
+ * Simple top navigation with links.
+ */
+export default function Navbar() {
+  return (
+    <header className={styles.navbar}>
+      <a href="#" className={styles.navLink}>
+        About Us
+      </a>
+      <a
+        href="https://dwong429.wixsite.com/gotit"
+        className={styles.navLink}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Blogs
+      </a>
+    </header>
+  );
+}
