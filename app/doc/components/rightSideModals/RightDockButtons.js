f6fbea71db675698cc99acb4f35f60178da5c1ef
EmmaJennings <ejennings13@my.bcit.ca>
2025-10-27 18:24:25 -0700

    Fixing right side button CSS

--- app/doc/components/rightSideModals/RightDockButtons.js
+++ app/doc/components/rightSideModals/RightDockButtons.js
@@ -13,14 +13,27 @@ export default function RightDockButtons({ timerBtnRef, onOpenTimer, onOpenA, on
             <IconButton
                 ref={timerBtnRef}
                 onClick={onOpenTimer}
-                sx={{ bgcolor: 'background.paper', boxShadow: 2, '&:hover': { boxShadow: 4 }, width: 48, height: 48, borderRadius: '14px' }}
+                sx={{ bgcolor: 'background.paper', boxShadow: 2, '&:hover': { boxShadow: 2 }, width: 48, height: 48, borderRadius: '14px' }}
             >
             <img src="/icons/timerLight.svg" alt="" width="40" height="40" />
             </IconButton>
         </Tooltip>
-        {/* <Tooltip title="Timer"><IconButton onClick={onOpenA}><img src="/icons/timerLight.svg" alt="timer" width="40" height="40" /></IconButton></Tooltip> */}
-        <Tooltip title="AI"><IconButton onClick={onOpenB}><img src="/icons/aiLight.svg" alt="ai" width="40" height="40" /></IconButton></Tooltip>
-        <Tooltip title="Vocab"><IconButton onClick={onOpenC}><img src="/icons/vocab.svg" alt="vocab" width="40" height="40" /></IconButton></Tooltip>
+        <Tooltip title="AI">
+            <IconButton 
+                onClick={onOpenB}
+                sx={{ bgcolor: 'background.paper', boxShadow: 2, '&:hover': { boxShadow: 2 }, width: 48, height: 48, borderRadius: '14px' }}
+            >
+                <img src="/icons/aiLight.svg" alt="ai" width="40" height="40" />
+            </IconButton>
+        </Tooltip>
+        <Tooltip title="Vocab">
+            <IconButton 
+                onClick={onOpenC}
+                sx={{ bgcolor: 'background.paper', boxShadow: 2, '&:hover': { boxShadow: 2 }, width: 48, height: 48, borderRadius: '14px' }}
+            >
+                <img src="/icons/vocab.svg" alt="vocab" width="40" height="40" />
+            </IconButton>
+        </Tooltip>
         </Box>
     );
 }
