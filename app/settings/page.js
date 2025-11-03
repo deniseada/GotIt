f6ac22d76ee2fd669659a39e03679fd4de5fc06f
BlankBozo <donaldwongplays@gmail.com>
2025-11-02 18:10:40 -0800

    PLEASE WORK

---
+++ app/settings/page.js
@@ -0,0 +1,16 @@
+"use client";
+
+import React from "react";
+import SettingsOverlay from "./components/SettingsOverlay";
+import useModal from "../doc/components/useModal";
+
+export default function SettingsPage() {
+  const settingsModal = useModal(true); // Open by default when navigating to settings
+
+  return (
+    <>
+      <SettingsOverlay open={settingsModal.open} onClose={settingsModal.onClose} />
+    </>
+  );
+}
+
