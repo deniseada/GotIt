da6d78ad381145c747ab984c8d86bdfc3bacda91
EmmaJennings <ejennings13@my.bcit.ca>
2025-11-19 18:32:00 -0800

    Added aiFeature page, testing and fixing errors with the simplification api call

---
+++ app/aiFeature/page.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+import Simplification from "./components/Simplification";
+
+
+export default function AIFeatures() {
+    return (
+        <div>
+            <Simplification />
+        </div>
+    );
+}
