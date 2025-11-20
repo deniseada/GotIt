b5ea8c1655b68296200e5bd90e942c9f5ff3e987
Micaela <micaela.valmores@gmail.com>
2025-11-20 08:43:31 -0800

    add the search icon in the search input

--- app/dashboard/components/searchBar.js
+++ app/dashboard/components/searchBar.js
@@ -1,13 +1,23 @@
 import styles from "../dashboard.module.css";
+import Image from "next/image";
 
 export default function SearchBar() {
   return (
     <div className={styles.searchBar}>
-      <input
-        type="text"
-        className={styles.searchInput}
-        placeholder="Search for books, sections, or sources"
-      />
+      <div className={styles.searchInputWrapper}>
+        <Image
+          src="/icons/searchIcon.svg"
+          alt="Search"
+          width={20}
+          height={20}
+          className={styles.searchIcon}
+        />
+        <input
+          type="text"
+          className={styles.searchInput}
+          placeholder="Search for books, sections, or sources"
+        />
+      </div>
     </div>
   );
 }
